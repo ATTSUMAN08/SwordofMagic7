8ba2c2bddc9f172e9f2e4fbf796669433b0647ce
ATTSUMAN08 <94741354+ATTSUMAN08@users.noreply.github.com>
2024-12-31 22:44:26 +0900

    fix

--- src/main/kotlin/net/somrpg/swordofmagic7/translater/GoogleIME.kt
+++ src/main/kotlin/net/somrpg/swordofmagic7/translater/GoogleIME.kt
@@ -2,20 +2,22 @@ package net.somrpg.swordofmagic7.translater
 
 import com.google.gson.Gson
 import com.google.gson.JsonArray
-import com.google.gson.JsonElement
 
 class GoogleIME {
 
-    protected constructor()
+    init {
+        throw UnsupportedOperationException("Utility Classはインスタンス化できません")
+    }
 
-    /**
-     * GoogleIMEの最初の変換候補を抽出して結合します
-     *
-     * @param json 変換元のJson形式の文字列
-     * @return 変換後の文字列
-     * @since 2.8.10
-     */
     companion object {
+
+        /**
+         * GoogleIMEの最初の変換候補を抽出して結合します
+         *
+         * @param json 変換元のJson形式の文字列
+         * @return 変換後の文字列
+         * @since 2.8.10
+         */
         @JvmStatic
         fun parseJson(json: String): String {
             val result = StringBuilder()
@@ -24,5 +26,6 @@ class GoogleIME {
             }
             return result.toString()
         }
+
     }
 }
--- src/main/kotlin/net/somrpg/swordofmagic7/translater/IMEConverter.kt
+++ src/main/kotlin/net/somrpg/swordofmagic7/translater/IMEConverter.kt
@@ -5,7 +5,7 @@ import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
-import java.net.URL
+import java.net.URI
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 
@@ -33,7 +33,7 @@ class IMEConverter {
             return try {
                 val baseurl = GOOGLE_IME_URL + URLEncoder.encode(org, StandardCharsets.UTF_8)
                 val encode = "UTF-8"
-                val url = URL(baseurl)
+                val url = URI.create(baseurl).toURL()
 
                 urlconn = url.openConnection() as HttpURLConnection
                 urlconn.requestMethod = "GET"
